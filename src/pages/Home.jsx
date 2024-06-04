0cd2beb864db91e8831a391281e1ec5029eca77d
dhiuet1329 <duchieu13072004@gmail.com>
2024-06-04 11:46:14 +0700

    update route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,24 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Home = ({ data }) => {
+  console.log("!1");
   console.log(data);
   return (
     <div>
-      <div className="row">
-        {data.map((item) => (
-          <div key={item.id} className="col-12 col-sm-6 col-md-4">
-            <div className="card">
-              <Link to={`/product-detail/${item.id}`}>
-                <img src={item.thumbnail} alt="" />
-              </Link>
-              <div className="content">
-                <Link to={`/product-detail/${item.id}`}>
-                  <h2 className="content-title">{item.title}</h2>
-                </Link>
-                <p>Price: {item.price}</p>
-                <button className="btn btn-primary">Mua ngay</button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <h1>Danh sách sản phẩm</h1>
+      {data.map((item) => (
+        <div key={item.id} className="card">
+          <Link to={`/product-detail/${item.id}`}>
+            <img src={item.thumbnail} alt="" />
+          </Link>
+          <Link to={`/product-detail/${item.id}`}>
+            <h2 className="content-title">{item.title}</h2>
+          </Link>
+          <p>{item.price}</p>
+          <p>{item.description}</p>
+          <button className="btn btn-primary">Add to cart</button>
+        </div>
+      ))}
     </div>
   );
 };
